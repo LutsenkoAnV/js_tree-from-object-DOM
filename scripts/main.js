ddffd2a52089c13a7cb17a4adde53e13d86e0125
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-02-04 15:37:06 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const keys = Object.keys(data);
+
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key of keys) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.append(li);
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length !== 0) {
+      createTree(li, data[key]);
+    }
+  }
+}
+
+createTree(tree, food);
